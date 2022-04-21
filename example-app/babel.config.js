b2f8275aa7321cf5e04689539546e47d05123af4
Jason Divock <jdivock@stripe.com>
2022-04-21 14:59:30 -0600

    parking

--- example-app/babel.config.js
+++ example-app/babel.config.js
@@ -1,7 +1,21 @@
+const pak = require('../package.json');
+const path = require('path');
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['module:react-native-dotenv']],
+    plugins: [
+      ['module:react-native-dotenv'],
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            [pak.name]: path.join(__dirname, '..', pak.source),
+          },
+        },
+      ],
+    ],
   };
 };
